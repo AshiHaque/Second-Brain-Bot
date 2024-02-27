885092ca0f3bdfc1f9d0f0b3b46796844a0f7bd2
Mir Ashiful Haque <mirashifulhaque@gmail.com>
2024-02-27 21:59:09 +0100

    minor update

--- main.py
+++ main.py
@@ -13,30 +13,30 @@ BOT_USERNAME: Final = '@A_SecondBrainBot'
 #Commands
 
 async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text('Hii! Tanks for starting me, I am your Second Brain')
+    await update.message.reply_text('Hii! Thanks for starting me :), I am your Second Brain!')
 
 async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text('Help')
+    await update.message.reply_text('For any issues contact my creator Ash! (@AshiHaque)')
 
-async def custom_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text('Custom')        
+async def howto_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text('Paste the url(s) for the rource(s) that you want to Save :)')        
 
 
 #Responses
 
-def handle_response(text: str) -> str:
-    processed = text.lower()  # Convert text to lowercase
+#def handle_response(text: str) -> str:
+    #processed = text.lower()  # Convert text to lowercase
     
-    if 'hello' in processed:
-        return 'Hiii!'
+    #if 'hello' in processed:
+        #return 'Hiii!'
     
-    if 'test1' in processed:
-        return 'Oneee'
+    #if 'test1' in processed:
+        #return 'Oneee'
     
-    if 'test2' in processed:
-        return 'Twooo'
+    #if 'test2' in processed:
+        #return 'Twooo'
     
-    return 'I do not understand what you wrote :('
+   # return 'I do not understand what you wrote :('
 
 
 async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -88,7 +88,7 @@ app.add_handler(CommandHandler('start', start_command))
 
 app.add_handler(CommandHandler('help', help_command))
 
-app.add_handler(CommandHandler('custom', custom_command))
+app.add_handler(CommandHandler('howto', howto_command))
 
 # Messages
 
