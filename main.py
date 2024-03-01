fe91f7e8d7dfb9438219c4e3e05c9231e87e397d
Mir Ashiful Haque <mirashifulhaque@gmail.com>
2024-03-01 20:43:20 +0100

    update

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from typing import Final
+rom typing import Final
 from telegram import Update
 from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes
 import os
